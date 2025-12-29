9e9e52de8f88ab33493e4a9ee0fe25f2fabc964f
champ <champdrm@gmail.com>
2025-12-29 22:37:38 +0700

    feat: add Chakra UI and Emotion dependencies for enhanced styling capabilities

--- src/components/ArticleSection.tsx
+++ src/components/ArticleSection.tsx
@@ -1,5 +1,5 @@
 import { Search } from 'lucide-react';
-import { Button } from './ui/Button';
+import { Button } from './ui/button';
 import { Input } from './ui/input';
 import {
     Select,
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
-import Github from "../img/Github_black.png"
-import linkedin from "../img/LinkedIN_black.png"
 import GoogleBlack from "../img/Google_black.png"
+import { Linkedin } from 'lucide-react';
+import { Github } from 'lucide-react';
+
 
 function Footer() {
     return (
         <>
             <div className="px-4 py-10 bg-brown-200 xl:flex xl:justify-between xl:items-center xl:px-32 xl:py-16">
                 <div className="flex justify-center items-center gap-4 mb-6 xl:mb-0">
-                    <p className="text-center text-body">Get in touch</p>
-                    <img src={linkedin} alt="linkedinIcon" className="w-6 h-6 grayscale" />
-                    <img src={Github} alt="GithubIcon" className="w-6 h-6 grayscale" />
+                    <div className="text-body xl:mt-1">Get in touch</div>
+                    <Linkedin />
+                    <Github />
                     <img src={GoogleBlack} alt="GoogleBlackIcon" className="w-6 h-6 grayscale" />
                 </div>
                 <div className="flex justify-center">
