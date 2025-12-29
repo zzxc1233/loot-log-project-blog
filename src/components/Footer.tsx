008f7370849480b39c9670b74f752a7644371027
champ <champdrm@gmail.com>
2025-12-29 18:03:33 +0700

    fix: adjust Footer layout with improved padding and responsive design adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ import GoogleBlack from "../img/Google_black.png"
 function Footer() {
     return (
         <>
-            <div className="p-6 bg-brown-200">
-                <div className="flex justify-center items-center gap-4">
+            <div className="px-4 py-10 bg-brown-200 xl:flex xl:justify-between xl:items-center xl:px-32 xl:py-16">
+                <div className="flex justify-center items-center gap-4 mb-6 xl:mb-0">
                     <p className="text-center text-body">Get in touch</p>
                     <img src={linkedin} alt="linkedinIcon" className="w-6 h-6 grayscale" />
                     <img src={Github} alt="GithubIcon" className="w-6 h-6 grayscale" />
