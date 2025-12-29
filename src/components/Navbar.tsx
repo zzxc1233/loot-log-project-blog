963ae97391a81935ae4ac7c2ed4ab0cd5b1103ef
champ <champdrm@gmail.com>
2025-12-29 15:32:11 +0700

    fix: replace burger bar image with Menu icon in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import logoHH from "../img/logo-hh.png"
-import burgerBar from "../img/burger-bar.png"
+import { Menu } from 'lucide-react';
 
 function NavBar() {
     return (
         <nav className='flex justify-between items-center py-3 px-6 border border-brown-300'>
             <img src={logoHH} alt="logo-HH" />
-            <img src={burgerBar} alt="burger-bar" className="w-6" />
+            <Menu />
         </nav>
     )
 }
