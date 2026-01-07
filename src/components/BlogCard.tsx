18bd023b4908cf1bcf555fece8bbdca90fe4d536
champ <champdrm@gmail.com>
2026-01-07 13:52:22 +0700

    fix: update BlogCard to use unique keys

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -7,8 +7,8 @@ export function BlogCard() {
     return (
         <section className="px-4 py-6">
             <div className="grid gap-6 xl:grid-cols-2">
-                {cards.map((card, index) => (
-                    <article key={index} className="flex flex-col">
+                {cards.map((card) => (
+                    <article key={card.id} className="flex flex-col">
                         <BlogCardUI {...card} />
                     </article>
                 ))}
