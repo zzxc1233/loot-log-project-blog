62e519638bd0f12156e994803432e78e0c7aacc9
champ <champdrm@gmail.com>
2026-01-13 16:10:16 +0700

    feat: add mobile navigation dropdown

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,18 +1,46 @@
-import logoHH from '../../assets/img/logo-hh.png'
-import { Menu } from 'lucide-react';
+import logoHH from "../../assets/img/logo-hh.png";
+import { Menu } from "lucide-react";
+
+// UI Components
 import { Button } from "../ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 function Navbar() {
     return (
-        <nav className='flex justify-between items-center py-3 px-6 border border-brown-300 xl:px-30'>
+        <nav className="flex justify-between items-center py-3 px-6 border border-brown-300 xl:px-30">
+            {/* Logo */}
             <img src={logoHH} alt="logo-HH" />
+
+            {/* Desktop Navigation (XL) */}
             <div className="hidden xl:flex gap-2">
                 <Button variant="login">Log in</Button>
                 <Button variant="signup">Sign up</Button>
             </div>
-            <Menu className="inline xl:hidden" />
+
+            {/* Mobile Navigation (Dropdown) */}
+            <div className="xl:hidden">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <button className="outline-none">
+                            <Menu />
+                        </button>
+                    </DropdownMenuTrigger>
+                    
+                    <DropdownMenuContent 
+                        className="w-screen rounded-none mt-4 flex flex-col gap-6 py-10 px-6"
+                        align="end"
+                    >
+                        <Button variant="login">Log in</Button>
+                        <Button variant="signup">Sign up</Button>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
