1ba72bed7c9f52ea157025087fac599be492b658
champ <champdrm@gmail.com>
2026-01-21 14:48:25 +0700

    feature(login): adds signup and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import BlogPostPage from './page/BlogPostPage'
 import PageNotFound from './page/PageNotFound'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { DataById } from './components/context/UseDataById'
+import { ValidationEmail } from './components/context/validationEmail'
 import { Toaster } from './components/ui/sonner'
+import SignupPage from './page/signup-page'
+import LoginPage from './page/login-page'
 
 function App() {
   return (
     <DataById>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog-post/:id" element={<BlogPostPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
+      <ValidationEmail>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog-post/:id" element={<BlogPostPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </ValidationEmail>
     </DataById>
   )
 }
---
+++ src/components/common/signup.tsx
@@ -0,0 +1,153 @@
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { ValidationEmailContext } from "../context/validationEmail";
+import { CircleCheck } from 'lucide-react';
+
+function SignUp() {
+
+    const navigate = useNavigate();
+
+    const [submit, setSubmit] = useState(false);
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState({
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const validationEmail = useContext(ValidationEmailContext);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        let isError = false;
+        let newError = { ...error };
+
+        if (!name.trim()) {
+            newError.name = "Name is required";
+            isError = true;
+        }
+        if (!username.trim()) {
+            newError.username = "Username is required";
+            isError = true;
+        }
+        if (!email.trim()) {
+            newError.email = "Email is required";
+            isError = true;
+        } else if (!validationEmail(email)) {
+            newError.email = "Email is invalid";
+            isError = true;
+        }
+        if (!password.trim()) {
+            newError.password = "Password is required";
+            isError = true;
+        }
+
+        setError(newError);
+        if (!isError) {
+            setSubmit(true);
+        }
+    }
+
+    return (
+        <>
+            <div className="bg-midnight-900 min-h-screen flex flex-col justify-center items-center px-4 sm:px-0">
+                <div className="flex flex-col gap-4 justify-center items-center py-8 sm:py-12 bg-midnight-800 w-full sm:w-3/4 md:w-1/2 mx-auto rounded-2xl border border-gold-400/30">
+                    <p className="text-headline-3 sm:text-headline font-semibold text-offwhite-200 pb-2 sm:pb-4">Sign Up</p>
+                    {!submit ? (
+                        <>
+                            <form
+                                onSubmit={handleSubmit}
+                                className="flex flex-col gap-4 sm:gap-6 w-11/12 sm:w-2/3"
+                            >
+                                <div className="flex flex-col gap-2">
+                                    <label htmlFor="name" className="text-offwhite-200">Name</label>
+                                    {error.name && <p className="text-body-3 text-red-500">{error.name}</p>}
+                                    <Input
+                                        type="text"
+                                        placeholder="Name"
+                                        className="mb-4"
+                                        value={name}
+                                        onChange={(e) => {
+                                            setName(e.target.value)
+                                            if (error.name) setError({ ...error, name: "" });
+                                        }}
+                                    />
+
+                                    <label htmlFor="username" className="text-offwhite-200">Username</label>
+                                    {error.username && <p className="text-body-3 text-red-500">{error.username}</p>}
+                                    <Input
+                                        type="text"
+                                        placeholder="Username"
+                                        className="mb-4"
+                                        value={username}
+                                        onChange={(e) => {
+                                            setUsername(e.target.value)
+                                            if (error.username) setError({ ...error, username: "" })
+                                        }}
+                                    />
+                                    <label htmlFor="email" className="text-offwhite-200">Email</label>
+                                    {error.email && <p className="text-body-3 text-red-500">{error.email}</p>}
+                                    <Input
+                                        type="email"
+                                        placeholder="Email"
+                                        className="mb-4"
+                                        value={email}
+                                        onChange={(e) => {
+                                            setEmail(e.target.value)
+                                            if (error.email) setError({ ...error, email: "" })
+                                        }}
+                                    />
+                                    <label htmlFor="password" className="text-offwhite-200">Password</label>
+                                    {error.password && <p className="text-body-3 text-red-500">{error.password}</p>}
+                                    <Input
+                                        type="password"
+                                        placeholder="Password"
+                                        className="mb-4"
+                                        value={password}
+                                        onChange={(e) => {
+                                            setPassword(e.target.value)
+                                            if (error.password) setError({ ...error, password: "" })
+                                        }}
+                                    />
+                                </div>
+                                <Button
+                                    type="submit"
+                                    className="w-full sm:w-1/3 m-auto bg-gold-600 rounded-2xl p-2"
+                                >
+                                    Sign Up
+                                </Button>
+                            </form>
+
+                            <div>
+                                <p className="text-body-3 sm:text-body-2 text-offwhite-200">Already have an account ?
+                                    <span className="text-gold-400 cursor-pointer hover:underline"
+                                        onClick={() => navigate("/login")}
+                                    > Log In</span>
+                                </p>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="flex flex-col gap-6 sm:gap-8 justify-center items-center py-4 px-4">
+                            <CircleCheck className="text-green" size={96}/>
+                            <p className="text-headline-4 sm:text-headline-3 font-semibold text-offwhite-200 text-center">Registration success</p>
+                            <Button
+                                onClick={() => navigate("/")}
+                                className="w-full sm:w-2/3 m-auto bg-gold-600 rounded-2xl p-4"
+                            >
+                                Continue
+                            </Button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SignUp
